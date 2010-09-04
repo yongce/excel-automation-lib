cd0f043f63164b93e238fd94c6e5fc83e484843d
tuyongce <tuyongce@e04aa62a-14a2-92d9-bf20-eeb9a5d2289a>
2010-09-04 15:25:47 +0000

    1. Add supports for alignment

--- trunk/ExcelAutomationLib/include/ExcelCell.h
+++ trunk/ExcelAutomationLib/include/ExcelCell.h
@@ -14,6 +14,7 @@
 #include "LibDef.h"
 #include "HandleBody.h"
 #include "StringUtil.h"
+#include "ExcelCommonTypes.h"
 
 
 // <begin> namespace
@@ -47,6 +48,20 @@ public:
     * @brief Return an object representing the font property of this cell
     */
     ExcelFont GetFont();
+
+    /*!
+    * @brief Set horizontal alignment of the cell
+    * @param [in] align Refer to ExcelHorizontalAlignment
+    * @return true if successful, otherwise false
+    */
+    bool SetHorizontalAlignment(ExcelHorizontalAlignment align);
+
+    /*!
+    * @brief Set vertical alignment of the cell
+    * @param [in] align Refer to ExcelVerticalAlignment
+    * @return true if successful, otherwise false
+    */
+    bool SetVerticalAlignment(ExcelVerticalAlignment align);
 
 private:
     friend class ExcelWorksheetImpl;  // which will call the following ctor
---
+++ trunk/ExcelAutomationLib/include/ExcelCommonTypes.h
@@ -0,0 +1,46 @@
+﻿/*!
+* @file    ExcelCommonTypes.h
+* @brief   Header file
+* @date    2010-09-04 21:33:10
+* @author  Tu Yongce <tuyongce@gmail.com>
+* @version $Id$
+*/
+
+
+#ifndef EXCELCOMMONTYPES_H_GUID_0A4BE828_D69E_4A79_A0C1_FA633D6C6035
+#define EXCELCOMMONTYPES_H_GUID_0A4BE828_D69E_4A79_A0C1_FA633D6C6035
+
+
+#include "LibDef.h"
+
+
+// <begin> namespace
+EXCEL_AUTOMATION_NAMESPACE_START
+
+
+// Constants for Horizontal Alignment
+enum ExcelHorizontalAlignment
+{
+    EHA_Left = 1,             // Left alignment
+    EHA_HCenter = 2,          // Horizontal center alignment: 
+    EHA_Right = 3,            // Right alignment
+};
+
+bool GetExcelConstant(ExcelHorizontalAlignment align, int &alignConstant);
+
+// Constants for Vertical Alignment
+enum ExcelVerticalAlignment
+{
+    EVA_Top = 1,              // Top alignment
+    EVA_VCenter = 2,          // Vertical center alignment
+    EVA_Bottom = 3,           // Bottom alignment
+};
+
+bool GetExcelConstant(ExcelVerticalAlignment align, int &alignConstant);
+
+// <end> namespace
+EXCEL_AUTOMATION_NAMESPACE_END
+
+
+#endif //EXCELCOMMONTYPES_H_GUID_0A4BE828_D69E_4A79_A0C1_FA633D6C6035
+
